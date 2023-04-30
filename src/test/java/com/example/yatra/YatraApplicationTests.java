ed5e5ba89da0cb8772e70c18524c9cd62fc218ce
test <anandmarkad4@gmail.com>
2023-04-30 17:06:47 +0530

    Code Push

---
+++ src/test/java/com/example/yatra/YatraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
