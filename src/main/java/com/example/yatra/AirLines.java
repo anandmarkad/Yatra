1d50abec15b87d7fe654de5263a884c190a43a19
test <anandmarkad4@gmail.com>
2023-06-16 11:06:05 +0530

    Change airline o/p.

--- src/main/java/com/example/yatra/AirLines.java
+++ src/main/java/com/example/yatra/AirLines.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AirLines {
     @GetMapping("/AirLines")
     public String getData() {
-        return "Airoplane mai CNG nahi hai....";
+        return "Thanks for using Airline ticket booking service....";
     }
 }
